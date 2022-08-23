5a326047b8dc981edaed5aa2505e323ecdc18f64
petrisoralexandru <kwixacademy@gmail.com>
2022-08-23 20:30:17 +0300

    Configure the Tailwind theme
    This commit consist in declarating some color palette to the theme and custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./ApolloClinic/**/*.cshtml"],
+  content: ["./ApolloClinic/**/*.cshtml"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue': {
+          DEFAULT: '#1363DF',
+          50: '#B1CDF8',
+          100: '#9EC1F7',
+          200: '#79A9F4',
+          300: '#5391F0',
+          400: '#2E79ED',
+          500: '#1363DF',
+          600: '#0F4CAB',
+          700: '#0A3578',
+          800: '#061E44',
+          900: '#010710'
+        },
+        'main-text': '#2e333c',
+        'hover-text': '#c0c1c4',
+      },
+      fontFamily: {
+        'tiempos': ['Tiempos Headline'],
+        'graphik': ['Graphik']
+      }
+    },
   },
   plugins: [],
 }
