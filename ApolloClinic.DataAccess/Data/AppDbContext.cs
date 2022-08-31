0a90c075df57ebd7964170b9ee2e1e2de860eae6
petrisoralexandru <kwixacademy@gmail.com>
2022-09-01 01:05:17 +0300

    Added SQL Server integration

---
+++ ApolloClinic.DataAccess/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApolloClinic.DataAccess.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+        
+        
+
+    }
+}
--- ApolloClinic/Program.cs
+++ ApolloClinic/Program.cs
@@ -1,7 +1,15 @@
+using ApolloClinic.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
